f25807a6dbc735cb34a0064801f6268c6019a595
Matteo Karl Donati <matteokarldonati@Matteos-MacBook-Pro.local>
2018-11-07 14:09:27 -0500

    small changes

---
+++ schedule_scraper.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_team_schedule(team, year):
+    """
+    :param team: string
+    :param year: string
+    :return: pandas.DataFrame containing the schedule of the team for the given year
+    """
+    base_url = "https://www.sports-reference.com/cbb/schools/"
+    url = base_url + team + '/' + year + '-schedule.html'
+
+    r = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
+
+    soup = BeautifulSoup(r.text, 'lxml')
+
+    for caption in soup.find_all('caption'):
+        if caption.get_text() == 'Schedule and Results Table':
+            table = caption.find_parent('table')
+
+    table_rows = table.find_all('tr')
+
+    th = table_rows[0].find_all('th')
+    columns = [i.text for i in th][1:]
+
+    data = []
+
+    for tr in table_rows:
+        td = tr.find_all('td')
+
+        row = [i.text for i in td]
+
+        if row:
+            data.append(row)
+
+    df = pd.DataFrame(data, index=np.arange(1, len(data) + 1), columns=columns)
+    return df
--- schools_scraper.py
+++ schools_scraper.py
@@ -1,20 +1,21 @@
 import requests
 from lxml import html
 
-def get_school(year):
-    '''
-    :param year: year you want to get the list of schools from (can be either int or string)
-    :return: list containing the names of the schools in alphqbetical order
-    '''
-    page = requests.get('https://www.sports-reference.com/cbb/seasons/'+str(year)+'-school-stats.html')
-    tree = html.fromstring(page.content)
-    hrefs = []
 
-    for i in range(1,500): # 500 is more than the numbers of expected colleges
+def get_schools(year):
+    """
+    :param year: string
+    :return: list containing the names of the schools in alphabetical order
+    """
+    page = requests.get('https://www.sports-reference.com/cbb/seasons/' + str(year) + '-school-stats.html')
+    tree = html.fromstring(page.content)
+    schools = []
+
+    for i in range(1, 500):  # 500 is more than the numbers of expected colleges
         try:
-            a = tree.xpath('//*[@id="basic_school_stats"]/tbody/tr['+str(i)+']/td[1]/a')
-            hrefs.append(a[0].get('href').split('/')[3])
+            a = tree.xpath('//*[@id="basic_school_stats"]/tbody/tr[' + str(i) + ']/td[1]/a')
+            schools.append(a[0].get('href').split('/')[3])
         except:
             pass
 
-    return hrefs
+    return schools

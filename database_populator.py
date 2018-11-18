ac15ea1c383295b65f3c49ccd140df34e0a8805c
Matteo Karl Donati <matteokarldonati@Matteos-MacBook-Pro.local>
2018-11-18 18:42:08 -0500

    n logic

--- database_populator.py
+++ database_populator.py
@@ -1,48 +1,59 @@
 import pandas as pd
-import os
-
-os.chdir('C:\\Users\\rdangelo\\Desktop\\ncaa_scraper\\data\\schedule')
 
 
-def matches_populator(file):
-    '''
+def matches_populator(file_name):
+    """
     transform a csv into database entities
 
     :return: void function
-    '''
+    """
+    df = pd.read_csv(file_name)
 
-    df = pd.read_csv(file)
+    team_1 = file_name.split('_')[1].replace('.csv', '')  # da mettere a posto
+    season = file_name.split('_')[0]
 
-    dn = pd.DataFrame(data=None,
-                      columns=['Team_1', 'Team_2', 'Team_1_points', 'Team_2_points', 'OT', 'Winner', 'Neutral',
-                               'Type', 'Arena', 'Date', 'Season'])
+    table = pd.DataFrame(data=None,
+                         columns=['Team_1', 'Team_2', 'Team_1_points', 'Team_2_points', 'OT', 'Winner', 'Neutral',
+                                  'Type', 'Arena', 'Date', 'Season'])
 
-    dn['OT'] = [1 if x == 'OT' else 0 for x in df['OT']]
-    dn['Neutral'] = [1 if x == 'N' else 0 for x in df[df.columns[3]]]
-    dn['Type'] = df['Type'].values
-    dn['Arena'] = df['Arena'].values
-    dn['Date'] = df['Date'].values
-    dn['Season'] = file.split('_')[0]
+    table['OT'] = [1 if x == 'OT' else 0 for x in df['OT']]
+    table['Neutral'] = [1 if x == 'N' else 0 for x in df[df.columns[3]]]
+    table['Type'] = df['Type'].values
+    table['Arena'] = df['Arena'].values
+    table['Date'] = df['Date'].values
+    table['Season'] = season
 
-    # Now I pu in order team 1 and team 2 with the following logic: team 1 is the one at the
-    for row in range(dn.shape[0]):
-        if df[df.columns[3]].iloc[row] == '@':
-            dn['Team_1'].iloc[row] = df['Opponent'].iloc[row]  ### BISOGNA PULIRLO E SOSTITUIRLO
-            dn['Team_2'].iloc[row] = file.split('_')[1].replace('.csv', '') ### DA METTERE A POSTO
-            dn['Team_1_points'].iloc[row] = df['Opp'].iloc[row]
-            dn['Team_2_points'].iloc[row] = df['Tm'].iloc[row]
-            dn['Winner'].iloc[row] = 0 if df[df.columns[6]].iloc[row] == 'W' else 1
+    # reorder team_1 and team_2 with the following logic: team 1 is the home team
+    # if the game was played on a neutral court, the names are ordered alphabetically
+    for row in range(table.shape[0]):
+        symbol = df[df.columns[3]].iloc[row]
+        team_2 = df['Opponent'].iloc[row]  # da pulire e sostituire
+        if symbol == '@':
+            table['Team_1'].iloc[row] = team_2
+            table['Team_2'].iloc[row] = team_1
+            table['Team_1_points'].iloc[row] = df['Opp'].iloc[row]
+            table['Team_2_points'].iloc[row] = df['Tm'].iloc[row]
+            table['Winner'].iloc[row] = 0 if df[df.columns[6]].iloc[row] == 'W' else 1
+
+        elif symbol == 'N':
+            if team_1 < team_2:
+                table['Team_1'].iloc[row] = team_1
+                table['Team_2'].iloc[row] = team_2
+                table['Team_1_points'].iloc[row] = df['Tm'].iloc[row]
+                table['Team_2_points'].iloc[row] = df['Opp'].iloc[row]
+                table['Winner'].iloc[row] = 1 if df[df.columns[6]].iloc[row] == 'W' else 0
+            else:
+                table['Team_1'].iloc[row] = team_2
+                table['Team_2'].iloc[row] = team_1
+                table['Team_1_points'].iloc[row] = df['Opp'].iloc[row]
+                table['Team_2_points'].iloc[row] = df['Tm'].iloc[row]
+                table['Winner'].iloc[row] = 0 if df[df.columns[6]].iloc[row] == 'W' else 1
 
         else:
-            dn['Team_1'].iloc[row] = file.split('_')[1].replace('.csv', '') ### DA METTERE A POSTO
-            dn['Team_2'].iloc[row] = df['Opponent'].iloc[row]  ### BISOGNA PULIRLO E SOSTITUIRLO
-            dn['Team_1_points'].iloc[row] = df['Tm'].iloc[row]
-            dn['Team_2_points'].iloc[row] = df['Opp'].iloc[row]
-            dn['Winner'].iloc[row] = 1 if df[df.columns[6]].iloc[row] == 'W' else 0
+            table['Team_1'].iloc[row] = team_1
+            table['Team_2'].iloc[row] = team_2
+            table['Team_1_points'].iloc[row] = df['Tm'].iloc[row]
+            table['Team_2_points'].iloc[row] = df['Opp'].iloc[row]
+            table['Winner'].iloc[row] = 1 if df[df.columns[6]].iloc[row] == 'W' else 0
 
-    return dn
-
-
-d = matches_populator('2006_akron.csv')
-
-print(d)
+    return table
